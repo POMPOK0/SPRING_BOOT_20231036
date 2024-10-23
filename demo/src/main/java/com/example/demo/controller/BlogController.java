9e244b804929c9d8b52ecf0712d42922b1a009f5
admin <eden20041228@gmail.com>
2024-10-23 14:47:54 +0900

    6주차 중간저장

--- demo/src/main/java/com/example/demo/controller/BlogController.java
+++ demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -3,10 +3,14 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
 
 
@@ -36,9 +40,21 @@ public class BlogController {
             model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
         } else {
             // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
-            return "error"; // 오류 처리 페이지로 연결
+            return "/error_page/article_error"; 
         }
         return "article_edit"; // .HTML 연결
     }
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    }
+
+    @DeleteMapping("/api/article_delete/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+        return "redirect:/article_list";
+}
 
 }
+
